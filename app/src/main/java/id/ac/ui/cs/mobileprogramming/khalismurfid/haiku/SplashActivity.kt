6090e936f7582b27343438465d8a77a9dd8c76b5
khalis murfid <khalismurfid@gmail.com>
2020-11-07 22:18:06 +0700

    Add haiku splash screen

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/khalismurfid/haiku/SplashActivity.kt
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.mobileprogramming.khalismurfid.haiku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
